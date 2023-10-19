1306539a4133f56a1b768470d186d04888dec58c
FamousLuisin <lufimelo@outlook.com>
2023-10-18 22:11:00 -0300

    outros meios de resolver o exercicio

--- Udemy/Section 3/exercicios/exercicio5.py
+++ Udemy/Section 3/exercicios/exercicio5.py
@@ -2,3 +2,9 @@ lista = ["Luis", "Noc", "Flinton"]
 
 for indice in range(len(lista)):
     print(indice, lista[indice])
+
+for i, item in enumerate(lista):
+    print(i, item)
+
+for item in enumerate(lista): # Devolve tuplas
+    print(item)
